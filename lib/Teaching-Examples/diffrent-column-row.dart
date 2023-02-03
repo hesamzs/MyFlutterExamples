c89dec94c26e873b6635f6ce60f6d7a7516403a7
HesamZs <javad.zarnooshe@gmail.com>
2023-02-03 16:03:02 +0330

    Fixed diffrent-column-row.dart !

--- lib/Teaching-Examples/diffrent-column-row.dart
+++ lib/Teaching-Examples/diffrent-column-row.dart
@@ -1,8 +1,9 @@
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 
 
 class DiffrentColumnAndRow extends StatefulWidget {
-  const DiffrentColumnAndRow({Key? key}) : super(key: key);
+  const DiffrentColumnAndRow({Key? key,required this.title}) : super(key: key);
+  final String title;
   @override
   State<DiffrentColumnAndRow> createState() => _DiffrentColumnAndRow();
 }
@@ -23,101 +24,43 @@ class _DiffrentColumnAndRow extends State<DiffrentColumnAndRow> {
     var size = MediaQuery.of(context).size;
     final double itemWidth = size.width;
 
-    return Padding(
-        padding: const EdgeInsets.all(22),
-        child: SizedBox(
-          height: 700,
-          child: Column(
-            children: [
-              const Padding(padding: EdgeInsets.only(top: 8, bottom: 8)),
-              SizedBox(
-                child: Text('Total Items (${columns.toString()})'),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 8)),
-              Expanded(
-                  child: ListView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    itemCount: columns,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        margin: const EdgeInsets.only(top: 4),
-                        width: itemWidth,
-                        height: 60,
-                        color: Colors.white70,
-                        child: Center(
-                          child: Text('Item ${(index + 1).toString()}'),
-                        ),
-                      );
-                    },
-                  )),
-              const Padding(padding: EdgeInsets.only(top: 8)),
-              SizedBox(
-                height: 45,
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        color: Colors.grey,
-                        child: IconButton(
-                          icon: const Icon(Icons.exposure_minus_1),
-                          onPressed: () {
-                            setState(() {
-                              columns = columns - 1;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        color: Colors.grey,
-                        child: IconButton(
-                          icon: const Icon(Icons.plus_one),
-                          onPressed: () {
-                            setState(() {
-                              columns = columns + 1;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(22),
+          child: SizedBox(
+            height: 700,
+            child: Column(
+              children: [
+                const Padding(padding: EdgeInsets.only(top: 8, bottom: 8)),
+                SizedBox(
+                  child: Text('Total Items (${columns.toString()})'),
                 ),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 8)),
-              const SizedBox(
-                height: 40,
-              ),
-              SizedBox(
-                child: Text('Total Items (${rows.toString()})'),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 8)),
-              SizedBox(
-                height: 100,
-                child: Row(
-                  children: [
-                    Expanded(
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          physics: const BouncingScrollPhysics(),
-                          itemCount: rows,
-                          itemBuilder: (context, index) {
-                            return Container(
-                              margin: const EdgeInsets.only(left: 4),
-                              width: itemWidth / 2.5,
-                              color: Colors.white70,
-                              child: Center(
-                                child: Text('Item ${(index + 1).toString()}'),
-                              ),
-                            );
-                          },
-                        ))
-                  ],
-                ),
-              ),
-              const Padding(padding: EdgeInsets.only(top: 8)),
-              SizedBox(
-                  height: 50,
+                const Padding(padding: EdgeInsets.only(top: 8)),
+                Expanded(
+                    child: ListView.builder(
+                      physics: const BouncingScrollPhysics(),
+                      itemCount: columns,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.only(top: 4),
+                          width: itemWidth,
+                          height: 60,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(4),
+                            color: Colors.white70,
+                          ),
+                          child: Center(
+                            child: Text('Item ${(index + 1).toString()}'),
+                          ),
+                        );
+                      },
+                    )),
+                const Padding(padding: EdgeInsets.only(top: 8)),
+                SizedBox(
+                  height: 45,
                   child: Row(
                     children: [
                       Expanded(
@@ -126,9 +69,11 @@ class _DiffrentColumnAndRow extends State<DiffrentColumnAndRow> {
                           child: IconButton(
                             icon: const Icon(Icons.exposure_minus_1),
                             onPressed: () {
-                              setState(() {
-                                rows = rows - 1;
-                              });
+                              if (columns > 0){
+                                setState(() {
+                                  columns -= 1;
+                                });
+                              }
                             },
                           ),
                         ),
@@ -140,20 +85,91 @@ class _DiffrentColumnAndRow extends State<DiffrentColumnAndRow> {
                             icon: const Icon(Icons.plus_one),
                             onPressed: () {
                               setState(() {
-                                rows = rows + 1;
+                                columns += 1;
                               });
                             },
                           ),
                         ),
                       ),
                     ],
-                  )),
-              const SizedBox(
-                height: 80,
-              ),
-            ],
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 8)),
+                const SizedBox(
+                  height: 40,
+                ),
+                SizedBox(
+                  child: Text('Total Items (${rows.toString()})'),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 8)),
+                SizedBox(
+                  height: 100,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            physics: const BouncingScrollPhysics(),
+                            itemCount: rows,
+                            itemBuilder: (context, index) {
+                              return Container(
+                                margin: const EdgeInsets.only(left: 4),
+                                width: itemWidth / 2.5,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(4),
+                                  color: Colors.white70,
+                                ),
+                                child: Center(
+                                  child: Text('Item ${(index + 1).toString()}'),
+                                ),
+                              );
+                            },
+                          ))
+                    ],
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 8)),
+                SizedBox(
+                    height: 50,
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            color: Colors.grey,
+                            child: IconButton(
+                              icon: const Icon(Icons.exposure_minus_1),
+                              onPressed: () {
+                                if (rows > 0){
+                                  setState(() {
+                                    rows -= 1;
+                                  });
+                                }
+                              },
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            color: Colors.grey,
+                            child: IconButton(
+                              icon: const Icon(Icons.plus_one),
+                              onPressed: () {
+                                setState(() {
+                                  rows += 1;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+                const SizedBox(
+                  height: 80,
+                ),
+              ],
+            ),
           ),
         ),
-      );
+    );
   }
 }

3bb5ba04795420c2132fb3d2cf19c1df606f3ff1
Hesam Zarnooshe <javad.zarnooshe@gmail.com>
2023-03-13 20:13:14 +0330

    Database Added !

--- lib/main.dart
+++ lib/main.dart
@@ -17,33 +17,7 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: Colors.grey[600],
           appBarTheme: AppBarTheme(backgroundColor: Colors.grey[850])),
       debugShowCheckedModeBanner: false,
-      home: Page(),
-    );
-  }
-}
-
-
-class Page extends StatefulWidget {
-  const Page({Key? key}) : super(key: key);
-
-  @override
-  State<Page> createState() => _PageState();
-}
-
-class _PageState extends State<Page> {
-
-  Database database = Database();
-
-  @override
-  void initState() {
-    super.initState();
-    database.DatabaseClass();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("Hello"),),
+      home: const FrontPage(),
     );
   }
 }
